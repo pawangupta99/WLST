3c87f7e8db9382fed95a31cebde66598e6d4ad51
EC2 Default User <ec2-user@ip-172-31-46-86.ec2.internal>
2020-04-28 06:33:27 +0000

    commiting in my branch

--- CreateDomain.py
+++ CreateDomain.py
@@ -3,7 +3,8 @@ if __name__ == '__main__':
     from wlstModule import *#@UnusedWildImport
     
 import os
-
+import sys
+from jinja2 import template
 
 
 #=======================================================================================
